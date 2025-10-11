d29c1a9585b08e8feeb18c0a3cdb6e7b749df3c2
HAJJINIHamza <hajjinihamza2@gmail.com>
2025-10-11 19:59:03 +0100

    Refactor: move simple app to simple_chatbot.py

---
+++ simple_chatbot.py
@@ -0,0 +1,33 @@
+import os 
+from langchain_groq import ChatGroq
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv() #Import environement variables 
+
+
+#Simple chatbot for starting
+def invoke_groq_llm(prompt:str):
+    groq_llm = ChatGroq(
+        groq_api_key = os.environ["GROQ_API_KEY"],
+        model_name = "llama-3.1-8b-instant", #Check groq website for other llm ids
+        temperature = 0.6  
+        ) 
+    reponse = groq_llm.predict(prompt)
+    return reponse
+
+#Streamlit app
+st.set_page_config(page_title = "Get answers to you question from groq")
+st.header("Chat with groq")
+#Get user input
+input = st.text_input("Your question", key ="input")
+#Get llm output
+response = invoke_groq_llm(input)
+submit = st.button("Get answer")
+
+if submit:
+    st.subheader("Groq's answer is :")
+    st.write(response)
+
+#Separator
+st.markdown("---")
